e28d069ba8cc0374671327e8f9270b4c1c88d969
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2026-05-14 20:48:08 +0530

    config files

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push('mjs');
+
+module.exports = config;
